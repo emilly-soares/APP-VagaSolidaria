11fbfe8223fe5faff101610b196fa598447c7acf
Emilly Soares Santos <soaressantosemilly05@gmail.com>
2024-09-05 08:48:55 -0400

    adjusting responsiveness

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,8 +5,7 @@ import emailIcon from '../assets/email-icon.svg';
 
 const StyledFooter = styled.footer`
   background-color: #F1F4F8;
-  padding-top: 3rem;
-  padding-bottom: 5rem;
+  padding: 3rem 1rem 5rem;
   color: #2D3150;
   text-align: center;
   width: 100%;
@@ -14,52 +13,49 @@ const StyledFooter = styled.footer`
 
 const FooterContent = styled.div`
   margin: 0 auto;
+  max-width: 1200px;
 `;
 
 const SupportText = styled.div`
   color: #EB801D;
   font-weight: bold;
   margin-bottom: 0.5rem;
+  font-size: 1.2rem;
 `;
 
 const SubscribeText = styled.div`
   font-weight: bold;
   margin-bottom: 0.5rem;
+  font-size: 1.2rem;
 `;
 
 const InfoText = styled.div`
   margin-bottom: 1rem;
+  font-size: 1rem;
 `;
 
-const SubscribeButton = styled.button`
-  background-color: #5187F0;
-  color: #fff;
-  padding: 0.5rem 1rem 1rem 0.5rem;
-  border: none;
-  border-radius: 15px;
-  cursor: pointer;
-  transition: background-color 0.3s ease;
-
-  &:hover {
-    background-color: #5298F1;
-  }
-`;
-
-  const EmailInputGroup = styled.div`
-  width: 30%;
+const EmailInputGroup = styled.div`
   display: flex;
-  justify-content: center; 
-  margin: 0 auto; 
-  border: 1px solid #ccc; 
-  border-radius: 20px; 
-  padding: 0.5rem; 
-  box-shadow: 0px 0px 5px #ccc; 
+  justify-content: center;
+  align-items: center;
+  width: 400px;  /* Define uma largura fixa */
+  margin: 0 auto;
+  padding: 0.5rem;
   margin-top: 2rem;
   margin-bottom: 10px;
+  border: 1px solid #ccc;
+  border-radius: 20px;
+  box-shadow: 0px 0px 5px #ccc;
   background-image: url(${emailIcon});
-  background-repeat: no-repeat; 
-  background-position: 10px center; 
+  background-repeat: no-repeat;
+  background-position: 10px center;
   padding-left: 40px;
+  
+  @media (max-width: 768px) {
+    width: 100%;  /* Deixa o input ocupar toda a largura disponível */
+    background-image: none;  /* Remove a imagem de fundo em telas menores */
+    padding-left: 20px;
+  }
 `;
 
 const EmailInput = styled.input`
@@ -69,12 +65,35 @@ const EmailInput = styled.input`
   outline:0;
   border:0;
   font-size: 16px;
+
+  @media (max-width: 768px) {
+    margin-bottom: 10px;
+  }
 `;
 
+const SubscribeButton = styled.button`
+  background-color: #5187F0;
+  color: #fff;
+  padding: 0.5rem 1rem;
+  border: none;
+  border-radius: 15px;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+  display: flex;
+  align-items: center;
+
+  &:hover {
+    background-color: #5298F1;
+  }
+
+  @media (max-width: 768px) {
+    justify-content: center;
+    padding: 0.5rem;
+  }
+`;
 
 const IconWrapper = styled.span`
   margin-right: 0.5rem;
-
 `;
 
 const Footer: React.FC = () => {
@@ -91,24 +110,22 @@ const Footer: React.FC = () => {
         <InfoText>Fique sabendo sobre as novas vagas</InfoText>
         <EmailInputGroup>
           <EmailInput
-            type="email"
-            placeholder="insira seu e-mail"
+          type="email"
+          placeholder="insira seu e-mail"
           />
 
           <SubscribeButton>
             <IconWrapper>
               <SiMinutemailer size={20} />
-            </IconWrapper >
+            </IconWrapper>
             Inscrever
           </SubscribeButton>
 
         </EmailInputGroup>
 
       </FooterContent>
-      
+
     </StyledFooter>
   );
 };
-
-
 export default Footer;
--- src/components/Home/style.tsx
+++ src/components/Home/style.tsx
@@ -6,9 +6,13 @@ export const Container = styled.div`
 `;
 
 export const Line = styled.img`
-    width: 50px; 
-    height: auto; 
-    margin-right: 0.5rem; 
+    width: 50px;
+    height: auto;
+    margin-right: 0.5rem;
+
+    @media (max-width: 768px) {
+        width: 30px;
+    }
 `;
 
 export const Comment = styled.img`
@@ -18,18 +22,35 @@ export const Comment = styled.img`
 export const PlatformSection = styled.div`
     width: 100%;
     padding-left: 5rem;
+
+    @media (max-width: 768px) {
+        padding-left: 2rem;
+    }
 `;
 
 export const PlatformFlex = styled.div`
     display: flex;
+    flex-direction: row;
+
+    @media (max-width: 768px) {
+        flex-direction: column;
+    }
 `;
 
 export const LeftSection = styled.div`
     width: 50%;
+
+    @media (max-width: 768px) {
+        width: 100%;
+    }
 `;
 
 export const ReviewsSection = styled.div`
     width: 60%;
+
+    @media (max-width: 768px) {
+        width: 100%;
+    }
 `;
 
 export const HomeTitle = styled.h1`
@@ -37,12 +58,22 @@ export const HomeTitle = styled.h1`
     font-weight: bold;
     margin-bottom: 0.5rem; 
     width: 70%;
+
+    @media (max-width: 768px) {
+        font-size: 32px;
+        width: 100%;
+    }
 `;
 
 export const Caption = styled.p`
     font-size: 25px;
     margin-bottom: 0.8rem; 
     width: 70%;
+
+    @media (max-width: 768px) {
+        font-size: 18px;
+        width: 100%;
+    }
 `;
 
 export const PlatformButton = styled.button`
@@ -54,6 +85,11 @@ export const PlatformButton = styled.button`
     cursor: pointer;
     font-size: 14px;
     justify-content: space-between;
+
+    @media (max-width: 768px) {
+        font-size: 12px;
+        padding: 0.6rem 0.8rem;
+    }
 `;
 
 export const HandWaveEmoji = styled.span`
@@ -65,7 +101,11 @@ export const Menu = styled.div`
     color: white;
     height: 550px; 
     background-color: #5187F0;
-    
+
+    @media (max-width: 768px) {
+        height: auto;
+        padding: 2rem 1rem;
+    }
 `;
 
 export const YellowButton = styled(Link)`
@@ -76,8 +116,14 @@ export const YellowButton = styled(Link)`
     border-radius: 5px;
     color: #2C2C2C;
     font-size: 16px;
+
     &:hover {
         background-color: #FFD43B;
+    }
+
+    @media (max-width: 768px) {
+        padding: 0.8rem 0.8rem;
+        font-size: 14px;
     }
 `;
 
@@ -92,10 +138,16 @@ export const RegisterButton = styled(Link)`
     height: 20%;
     font-size: 16px;
     margin-top: 2rem;
+
     &:hover {
         background-color: #FFD43B;
     }
-    `;
+
+    @media (max-width: 768px) {
+        padding: 0.8rem 0.8rem;
+        font-size: 14px;
+    }
+`;
 
 export const BenefitsSection = styled.div`
     width: 100%;
@@ -104,30 +156,58 @@ export const BenefitsSection = styled.div`
     margin-bottom: 3rem;
     font-size: 18px;
     color: #515151;
+
+    @media (max-width: 768px) {
+        margin-left: 1rem;
+        margin-top: 2rem;
+        margin-bottom: 2rem;
+        font-size: 16px;
+    }
 `;
 
 export const FirstSection = styled.div`
     width: 50%;
-    display:flex;
+    display: flex;
     margin-bottom: 2rem;
+
+    @media (max-width: 768px) {
+        width: 100%;
+    }
 `;
 
 export const Benefit = styled.img`
     margin-top: 7rem;
     margin-left: 1rem;
     width: 95%;
+
+    @media (max-width: 768px) {
+        width: 100%;
+        margin-top: 2rem;
+        margin-left: 0;
+    }
 `;
 
 export const CompanyBenefits = styled.img`
     margin-top: 10rem;
     margin-left: 1rem;
     width: 98%;
+
+    @media (max-width: 768px) {
+        margin-top: 5rem;
+        margin-left: 0;
+        width: 100%;
+    }
 `;
 
 export const RegisterSection = styled.img`
     margin-top: 10rem;
     margin-left: 1rem;
     width: 100%;
+
+    @media (max-width: 768px) {
+        margin-top: 5rem;
+        margin-left: 0;
+    }
 `;
 
 export const PopupContainer = styled.div`
@@ -146,6 +226,11 @@ export const PopupContainer = styled.div`
     &:hover {
         transform: scale(1.1);
     }
+
+    @media (max-width: 768px) {
+        bottom: 10px;
+        right: 5px;
+    }
 `;
 
 export const WhatsAppIcon = styled.img`
@@ -153,6 +238,12 @@ export const WhatsAppIcon = styled.img`
     height: 30px;
     margin-right: 10px;
     margin-top: 10px;
+
+    @media (max-width: 768px) {
+        width: 25px;
+        height: 25px;
+        margin-right: 5px;
+    }
 `;
 
 export const WhatsAppLink = styled.a`
@@ -161,4 +252,8 @@ export const WhatsAppLink = styled.a`
     font-size: 16px;
     font-weight: bold;
     padding-bottom: 10px;
+
+    @media (max-width: 768px) {
+        font-size: 14px;
+    }
 `;
